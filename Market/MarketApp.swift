36a4f1c07f888f8647b192e320e1e49d6a8a3cf2
Roma <moshkovcevroma@gmail.com>
2022-12-18 15:10:22 +0700

    Initial Commit

---
+++ Market/MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketApp.swift
+//  Market
+//
+//  Created by Роман Мошковцев on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
